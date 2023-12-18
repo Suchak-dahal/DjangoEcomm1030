d68922f0acfe4b483bc0574738f0a45c3473dab9
Suchak Dahal <sharmasupreme111@gmail.com>
2023-12-18 11:33:08 +0545

    all model

--- home/models.py
+++ home/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=200)
@@ -23,6 +24,7 @@ class Ad(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(upload_to='media')
     rank = models.IntegerField()
+
     def __str__(self):
         return self.title
 
@@ -35,3 +37,31 @@ class Brand(models.Model):
     def __str__(self):
         return self.title
 
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=500)
+    post = models.CharField(max_length=500)
+    rate = models.IntegerField()
+    image = models.ImageField(upload_to='media')
+    comment = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ContactInfo(models.Model):
+    address = models.CharField(max_length=300)
+    email = models.EmailField(max_length=500)
+    phone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.address
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=500)
+    subject = models.TextField(blank=True)
+    message = models.TextField(blank=True)
+    def __str__(self):
+        return self.name
